40d1bfe4c81a6f6a33843d7e1a9b61616f5c101f
RPSPandora <Randhi.pp@gmail.com>
2017-01-27 13:34:24 +0700

    Add files via upload

---
+++ InstaBulkUpload.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Use text editor to edit the script and type in valid Instagram username/password
+
+import os
+import time
+import random
+from os import listdir
+from os.path import isfile, join
+from random import randint
+from InstagramAPI import InstagramAPI
+
+PhotoPath = "~/igphoto/" # Change Directory to Folder with Pics that you want to upload
+IGUSER    = "IGUsername" # Change to your Instagram USERNAME
+PASSWD    = "IGPassword" # Change to your Instagram Password
+# Change to your Photo Hashtag
+IGCaption = "Your Caption Here #hashtag"
+
+os.chdir(PhotoPath)
+ListFiles = [f for f in listdir(PhotoPath) if isfile(join(PhotoPath, f))]
+print ("Total Photo in this folder:" + str (len(ListFiles)))
+
+#Start Login and Uploading Photo
+igapi = InstagramAPI(IGUSER,PASSWD)
+igapi.login() # login
+
+for i in range(len(ListFiles)):
+    photo = ListFiles[i]
+    print ("Progress :" + str([i]) + " of " + str(len(ListFiles)))
+    print ("Now Uploading this photo to instagram: " + photo)
+    igapi.uploadPhoto(photo,caption=IGCaption,upload_id=None)
+    # sleep for random between 30 - 300s
+    n = randint(60,120)
+    print ("Sleep upload for seconds: " + str(n))
+    time.sleep(n)
